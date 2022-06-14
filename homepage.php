42617cc2af9fdbc13329f0038d5ad5f78c80bbb4
Jiheonie <hoangtunguyen2002@gmail.com>
2022-06-14 22:24:07 +0700

    add header, footer include

--- homepage.php
+++ homepage.php
@@ -5,8 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="header_style.css">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="./header_style.css">
+    <link rel="stylesheet" href="./footer_style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
@@ -145,61 +146,7 @@
     </div>
 
     <!-- FOOTER SECTION -->
-    <div class="footer">
-      <footer class="footer__container">
-        <div class="footer__container-row">
-          <div class="footer__container-row-section">
-            <h5 class="footer__container-row-section-title">Section</h5>
-            <ul class="footer__container-row-section-items">
-              <li class="footer__container-row-section-item"><a href="#">Home</a></li>
-              <li class="footer__container-row-section-item"><a href="#">Features</a></li>
-              <li class="footer__container-row-section-item"><a href="#">Pricing</a></li>
-              <li class="footer__container-row-section-item"><a href="#">FAQs</a></li>
-              <li class="footer__container-row-section-item"><a href="#">About</a></li>
-            </ul>
-          </div>
-          <div class="footer__container-row-section">
-            <h5 class="footer__container-row-section-title">Section</h5>
-            <ul class="footer__container-row-section-items">
-              <li class="footer__container-row-section-item"><a href="#">Home</a></li>
-              <li class="footer__container-row-section-item"><a href="#">Features</a></li>
-              <li class="footer__container-row-section-item"><a href="#">Pricing</a></li>
-              <li class="footer__container-row-section-item"><a href="#">FAQs</a></li>
-              <li class="footer__container-row-section-item"><a href="#">About</a></li>
-            </ul>
-          </div>
-          <div class="footer__container-row-section">
-            <h5 class="footer__container-row-section-title">Section</h5>
-            <ul class="footer__container-row-section-items">
-              <li class="footer__container-row-section-item"><a href="#">Home</a></li>
-              <li class="footer__container-row-section-item"><a href="#">Features</a></li>
-              <li class="footer__container-row-section-item"><a href="#">Pricing</a></li>
-              <li class="footer__container-row-section-item"><a href="#">FAQs</a></li>
-              <li class="footer__container-row-section-item"><a href="#">About</a></li>
-            </ul>
-          </div>
-          <div class="footer__container-row-subscribe">
-            <form>
-              <h5 class="footer__container-row-subscribe-title">Subscribe to our newsletter</h5>
-              <p class="footer__container-row-subscribe-description">Monthly digest of what's new and exciting from us.</p>
-              <div class="footer__container-row-subscribe-email">
-                <input id="newsletter1" type="text" class="footer__container-row-subscribe-input" placeholder="Email address">
-                <button class="btn btn--primary" type="button">Subscribe</button>
-              </div>
-            </form>
-          </div>
-        </div>
-
-        <div class="footer__container-under">
-          <p class="footer__container-under-description">© 2022 Company, Inc. All rights reserved.</p>
-          <ul class="footer__container-under-media">
-            <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
-            <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
-            <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
-          </ul>
-        </div>
-      </footer>
-    </div>
+    <?php include './include/footer.php' ?>
 
     <script src="app.js"></script>
     <!-- add cmt -->
---
+++ include/footer.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+<div class="footer">
+      <footer class="footer__container">
+        <div class="footer__container-row">
+          <div class="footer__container-row-section">
+            <h5 class="footer__container-row-section-title">Section</h5>
+            <ul class="footer__container-row-section-items">
+              <li class="footer__container-row-section-item"><a href="#">Home</a></li>
+              <li class="footer__container-row-section-item"><a href="#">Features</a></li>
+              <li class="footer__container-row-section-item"><a href="#">Pricing</a></li>
+              <li class="footer__container-row-section-item"><a href="#">FAQs</a></li>
+              <li class="footer__container-row-section-item"><a href="#">About</a></li>
+            </ul>
+          </div>
+          <div class="footer__container-row-section">
+            <h5 class="footer__container-row-section-title">Section</h5>
+            <ul class="footer__container-row-section-items">
+              <li class="footer__container-row-section-item"><a href="#">Home</a></li>
+              <li class="footer__container-row-section-item"><a href="#">Features</a></li>
+              <li class="footer__container-row-section-item"><a href="#">Pricing</a></li>
+              <li class="footer__container-row-section-item"><a href="#">FAQs</a></li>
+              <li class="footer__container-row-section-item"><a href="#">About</a></li>
+            </ul>
+          </div>
+          <div class="footer__container-row-section">
+            <h5 class="footer__container-row-section-title">Section</h5>
+            <ul class="footer__container-row-section-items">
+              <li class="footer__container-row-section-item"><a href="#">Home</a></li>
+              <li class="footer__container-row-section-item"><a href="#">Features</a></li>
+              <li class="footer__container-row-section-item"><a href="#">Pricing</a></li>
+              <li class="footer__container-row-section-item"><a href="#">FAQs</a></li>
+              <li class="footer__container-row-section-item"><a href="#">About</a></li>
+            </ul>
+          </div>
+          <div class="footer__container-row-subscribe">
+            <form>
+              <h5 class="footer__container-row-subscribe-title">Subscribe to our newsletter</h5>
+              <p class="footer__container-row-subscribe-description">Monthly digest of what's new and exciting from us.</p>
+              <div class="footer__container-row-subscribe-email">
+                <input id="newsletter1" type="text" class="footer__container-row-subscribe-input" placeholder="Email address">
+                <button class="btn btn--primary" type="button">Subscribe</button>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <div class="footer__container-under">
+          <p class="footer__container-under-description">© 2022 Company, Inc. All rights reserved.</p>
+          <ul class="footer__container-under-media">
+            <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+            <li><a href="#"><i class="fa-brands fa-facebook"></i></a></li>
+          </ul>
+        </div>
+      </footer>
+    </div>    
+</body>
+</html>
