0de4d96aa7f2f105250c396d9a3803279d87d965
Jiheonie <hoangtunguyen2002@gmail.com>
2022-06-14 22:09:33 +0700

    add include

--- homepage.php
+++ homepage.php
@@ -6,56 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="header_style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
     <!-- HEADER -->
-    <header>
-      <div class="header__menu--phone">
-        <i class="fa-solid fa-bars"></i>
-      </div>
-      <div class="header__logo">
-        <a href="/"><img class="header__logo-img" src="./images/logo-coolmate.png" alt="Logo Coolmate"></a>
-      </div>
-      <div class="header__menu">
-        <ul class="header__menu-list">
-          <li class="header__menu-item header__menu-item--focus">
-            <a class="header__menu-item-link" href="#">Outlet</a>
-          </li>
-          <li class="header__menu-item">
-            <a class="header__menu-item-link" href="#">
-            Đồ lót định kỳ</a>
-          </li>
-          <li class="header__menu-item">
-            <a class="header__menu-item-link" href="#">
-            Sản phẩm</a>
-          </li>
-          <li class="header__menu-item">
-            <a class="header__menu-item-link" href="#">
-            In áo công ty</a>
-          </li>
-          <li class="header__menu-item">
-            <a class="header__menu-item-link" href="./introduce.html">
-            Về Coolmate</a>
-          </li>
-          <li class="header__menu-item">
-            <a class="header__menu-item-link" href="#">
-            Chọn Size</a>
-          </li>
-        </ul>
-      </div>
-      <div class="header__actions">
-        <div class="header__actions-item">
-          <i class="headers_actions-item-icon fa-solid fa-magnifying-glass"></i>
-        </div>
-        <div class="header__actions-item">
-          <i class="headers_actions-item-icon fa-solid fa-user"></i>
-        </div>
-        <div class="header__actions-item">
-          <i class="headers_actions-item-icon fa-solid fa-bag-shopping"></i>
-        </div>
-      </div>
-    </header>
+    <?php include './include/header.php' ?>
 
     <!-- HERO SECTION -->
     <div class="hero">
